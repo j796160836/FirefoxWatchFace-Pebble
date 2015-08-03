ce9203bff77368fb86d4b922379dd813fefb2358
Johnny Sung <j796160836@gmail.com>
2015-08-03 11:47:13 +0800

    First commit

---
+++ FireFoxWatchFace-Black/src/FireFoxWatchFace-Black.c
@@ -0,0 +1,94 @@
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *s_time_layer;
+static GBitmap *s_bitmap;
+static BitmapLayer *s_bitmap_layer;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BG);
+
+  s_bitmap_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+#ifdef PBL_PLATFORM_APLITE
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpAssign);
+#elif PBL_PLATFORM_BASALT
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
+#endif
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+
+
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 59, 144, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  update_time();
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(s_time_layer);
+  bitmap_layer_destroy(s_bitmap_layer);
+  gbitmap_destroy(s_bitmap);
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_background_color(window, COLOR_FALLBACK(GColorBlack, GColorBlack));
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
